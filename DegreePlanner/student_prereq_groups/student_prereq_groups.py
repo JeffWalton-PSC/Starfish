fe146ae246cc35718eaaccd9a38f77f13c570f07
Jeff Walton <jwalton@paulsmiths.edu>
2018-06-28 17:16:21 -0400

    DegreePlanner student_prereq_groups.py

--- DegreePlanner/student_prereq_groups/student_prereq_groups.py
+++ DegreePlanner/student_prereq_groups/student_prereq_groups.py
@@ -12,65 +12,70 @@ import util
 today = date.today()
 today_str = today.strftime('%Y%m%d')
 
-sql_str = "SELECT * FROM TRANSCRIPTDETAIL WHERE " + \
-          "CREDIT_TYPE = 'TRAN' "
-df_td = pd.read_sql_query(sql_str, connection)
+sql_str = "SELECT PEOPLE_CODE_ID, ACADEMIC_YEAR, ACADEMIC_TERM, ACADEMIC_SESSION, " + \
+          "CREDITS, PRIMARY_FLAG, CLASS_LEVEL " + \
+          "FROM ACADEMIC WHERE " + \
+          "ACADEMIC_SESSION = '' " + \
+          "AND PRIMARY_FLAG = 'Y' " + \
+          "AND CREDITS > 0 "
+df_aca = pd.read_sql_query(sql_str, connection)
 
-df_td = df_td[['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM', 'ACADEMIC_SESSION', 
-             'EVENT_ID', 'EVENT_SUB_TYPE', 'SECTION', 'EVENT_MED_NAME', 
-             'ORG_CODE_ID', 'CREDIT_TYPE', 'CREDIT', 'FINAL_GRADE', 
-             'REFERENCE_EVENT_ID', 'REFERENCE_SUB_TYPE', 
-             ]]
+df_aca = df_aca[['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM', 'ACADEMIC_SESSION', 
+                 'CREDITS', 'PRIMARY_FLAG', 'CLASS_LEVEL', 
+                 ]]
 
-# keep transfer records for active students
-df = util.apply_active(in_df=df_td)
+sql_str = "SELECT PEOPLE_CODE_ID, ACADEMIC_YEAR, ACADEMIC_TERM, ACADEMIC_SESSION, " + \
+          "RECORD_TYPE, TOTAL_CREDITS, GPA " + \
+          "FROM TRANSCRIPTGPA WHERE " + \
+          "RECORD_TYPE = 'O' " + \
+          "AND TOTAL_CREDITS >= 0 "
+df_tgpa = pd.read_sql_query(sql_str, connection)
 
-crs_id = (lambda c: (str(c['EVENT_ID']).replace(' ', '') +
-                     str(c['EVENT_SUB_TYPE']).upper())
-          if ((c['EVENT_SUB_TYPE'] == 'LAB') | (c['EVENT_SUB_TYPE'] == 'SI'))
-          else (str(c['EVENT_ID']).replace(' ', ''))
-          )
-df.loc[:, 'transfer_course_number'] = df.apply(crs_id, axis=1)
+df_tgpa = df_tgpa[['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM', 'ACADEMIC_SESSION', 
+                 'RECORD_TYPE',
+                 ]]
 
-tr_section_id = (lambda c: (c['EVENT_ID'] + '.' +
-                            c['EVENT_SUB_TYPE'] + '.Transfer'
-                           )
-                 if ((c['ACADEMIC_YEAR'] == '1999') | (c['ACADEMIC_YEAR'] == '2004'))
-                 else (c['EVENT_ID'] + '.' +
-                       c['EVENT_SUB_TYPE'] + '.' +
-                       c['ACADEMIC_YEAR'] + '.' +
-                       c['ACADEMIC_TERM'].title() + '.TR'
-                      )
-                )
-df.loc[:, 'transfer_course_section_number'] = df.apply(tr_section_id, axis=1)
-df.loc[:, 'ag_grading_type'] = 'P/F'
-df.loc[:, 'ag_status'] = 'TRANSFER'
+df = pd.merge(df_aca, df_tgpa, 
+              on=['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM', 'ACADEMIC_SESSION'],
+              how='left')
 
-df = df.rename(columns={'PEOPLE_CODE_ID': 'student_integration_id',
-                        'CREDIT': 'credits',
-                        'EVENT_MED_NAME': 'course_title',
-                        'ACADEMIC_YEAR': 'term_year',
-                        'ACADEMIC_TERM': 'term_season',
+# keep records for active students
+df = util.apply_active(in_df=df)
+
+# drop not completed terms
+df = df[(~df['TOTAL_CREDITS'].isnull())]
+
+# find the latest year
+df = df[(~df['ACADEMIC_YEAR'].isnull())]
+df['ACADEMIC_YEAR'] = (pd.to_numeric(df['ACADEMIC_YEAR'], errors='coerce'))
+df_seq = pd.DataFrame([{'term': 'SPRING', 'seq': 1},
+                       {'term': 'SUMMER', 'seq': 2},
+                       {'term': 'FALL', 'seq': 3}])
+df = pd.merge(df, df_seq, left_on='ACADEMIC_TERM', right_on='term', how='left')
+df['term_seq'] = df['ACADEMIC_YEAR'] * 100 + df['seq']
+df = (df.loc[df.reset_index()
+               .groupby(['PEOPLE_CODE_ID'])['term_seq']
+               .idxmax()])
+
+df.loc[:,'prereq_group_identifier'] = 'FRESHMAN'
+df.loc[(df['TOTAL_CREDITS'] >= 30),'prereq_group_identifier'] = 'SOPHOMORE'
+df.loc[(df['TOTAL_CREDITS'] >= 60),'prereq_group_identifier'] = 'JUNIOR'
+df.loc[(df['TOTAL_CREDITS'] >= 90),'prereq_group_identifier'] = 'SENIOR'
+
+df = df.rename(columns={
+                        'PEOPLE_CODE_ID': 'student_integration_id',
                        })
 
-tr_grade = (lambda c: 'P'
-            if (c['FINAL_GRADE'] == 'TR')
-            else 'NG'
-           )
-df.loc[:, 'ag_grade'] = df.apply(tr_grade, axis=1)
-df = df[~df['ag_grade'].isnull()]
 
-df = df.loc[:, ['student_integration_id', 'transfer_course_number',
-                'transfer_course_section_number',
-                'ag_grade', 'ag_grading_type', 'ag_status',
-                'credits', ]]
+df = df.loc[:, ['student_integration_id', 'prereq_group_identifier',
+               ]]
 
 df = (df.sort_values(['student_integration_id', 
-                      'transfer_course_section_number'])
+                      'prereq_group_identifier'])
         .drop_duplicates(['student_integration_id', 
-                          'transfer_course_section_number'],
+                          'prereq_group_identifier'],
                          keep='last')
      )
 
-fn_output = f'{today_str}_student_transfer_records.txt'
+fn_output = f'{today_str}_student_prereq_groups.txt'
 df.to_csv(fn_output, index=False)
