5ba0387f75ff5518c58783b20a297da7519a7f54
Jeff Walton <jwalton@paulsmiths.edu>
2018-09-19 17:31:08 -0400

    Starfish Exporter add combine_exporter_files.py

--- Exporter/combine_exporter_files/combine_exporter_files.py
+++ Exporter/combine_exporter_files/combine_exporter_files.py
@@ -1,26 +1,28 @@
 """
-Data file for Starfish's DegreePlanner.
-Creates pre-requisite groups based on geneds (SC-I, WC-F, etc.).
+Combines daily difference files from Starfish Exporter into one file.
 """
 import pandas as pd
-from datetime import date
 from pathlib import Path
-import os
 
-prereq_output_path = Path(r'\\psc-data\E\Applications\Starfish\Files\workingfiles\student_prereq_groups\student_prereq_groups.txt')
-prereq_input_files = []
+exporter_path = Path(r'\\psc-data\E\Applications\Starfish\Files\exporter')
+output_path = Path(r'\\psc-data\E\Data\exporter_data')
 
-def find_prereq_files():
-    prereq_files_path = Path(r'\\psc-data\E\Applications\Starfish\Files\workingfiles\student_prereq_groups')
-    import os
-    for root, dirs, files in os.walk(prereq_files_path):
-        for file in files:
-            if file.endswith("student_prereq_groups.txt") and file != "student_prereq_groups.txt":
-                prereq_input_files.append(os.path.join(root, file))
+files = exporter_path.glob('*.csv')
 
-#find files that end with "student_prereq_groups.txt"
-find_prereq_files()
-#combine them
-combined_csv = pd.concat( [ pd.read_csv(f) for f in prereq_input_files ] )
-#output them to a new file
-combined_csv.to_csv( prereq_output_path, index=False )
+file_types = set()
+for f in files:
+    ft = f.stem.split('-')[0]
+    file_types.add(ft)
+
+for ft in file_types:
+    files = exporter_path.glob(ft + '*.csv')
+    combined_df = pd.concat( [ pd.read_csv(f) for f in files ] )
+    print(ft, combined_df.shape)
+    #print(combined_df.columns)
+    print(combined_df.info())
+    combined_df = combined_df.drop_duplicates()
+    print(ft, combined_df.shape)
+
+    outfile = output_path / (ft + '.csv')
+    print(outfile)
+    combined_df.to_csv(outfile, index=False)
